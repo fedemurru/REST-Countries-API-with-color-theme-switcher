f829d9e99aef3ba5f3697ca2e5c9c50e4cae2da9
Federico.Murru <djfedemurru@icloud.com>
2024-08-09 08:26:59 +0100

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import "./App.css";
+import { useState } from "react";
 
 function App() {
+	const [inputField, setInputField] = useState(undefined);
+
 	return (
 		<>
-			<p className="read-the-docs">Hello</p>
+			<div className="container">
+				<form>
+					<div className="inputField">
+						<input
+							type="search"
+							placeholder="Search for a country..."
+							value={inputField}
+							onChange={(e) => {
+								setInputField(e.target.value);
+							}}
+						/>
+						<i className="fas fa-search"></i>
+					</div>
+					<select id="region" name="region">
+						<option value="All" defaultValue>
+							All
+						</option>
+						<option value="Africa">Africa</option>
+						<option value="Americas">America</option>
+						<option value="Asia">Asia</option>
+						<option value="Europe">Europe</option>
+						<option value="Oceania">Oceania</option>
+					</select>
+				</form>
+			</div>
 		</>
 	);
 }
